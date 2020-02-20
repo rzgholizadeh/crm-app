e665d3690da25c46dfb4542aef3f23ec778821f2
Reza <rzgholizadeh@gmail.com>
2020-02-19 23:35:26 -0700

    Refactor the architecture to add service layer for DAOs
    Added service layer between DAO and controller to practice the better architecture.

---
+++ crmapp/src/main/java/com/mrghz/spring/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.mrghz.spring.service;
+
+import java.util.List;
+
+import com.mrghz.spring.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+}
---
+++ crmapp/src/main/java/com/mrghz/spring/service/CustomerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.mrghz.spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mrghz.spring.dao.CustomerDAO;
+import com.mrghz.spring.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// need to inject customer DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+
+	@Transactional
+	@Override
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+}
