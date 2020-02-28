3baaf86909f04a0d854122f08050dad19ccacb8f
Reza <rzgholizadeh@gmail.com>
2020-02-27 18:12:39 -0700

    test: Refactor CustomerServiceImplTest
    Developed the first version of test for deleteById() method.

--- crmapp/src/test/java/com/mrghz/crmapp/controller/CustomerControllerTest.java
+++ crmapp/src/test/java/com/mrghz/crmapp/controller/CustomerControllerTest.java
@@ -4,8 +4,22 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import mrghz.crmapp.controller.CustomerController;
+import mrghz.crmapp.service.CustomerService;
+
+@ExtendWith(MockitoExtension.class)
 class CustomerControllerTest {
+
+	@Mock
+	CustomerService customerServicelMock;
+
+	@InjectMocks
+	CustomerController theController;
 
 	@BeforeEach
 	void setUp() throws Exception {
--- crmapp/src/test/java/com/mrghz/crmapp/service/CustomerServiceImplTest.java
+++ crmapp/src/test/java/com/mrghz/crmapp/service/CustomerServiceImplTest.java
@@ -1,34 +1,53 @@
 package com.mrghz.crmapp.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import mrghz.crmapp.dao.CustomerRepository;
+import mrghz.crmapp.service.CustomerServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceImplTest {
+
+	@Mock
+	CustomerRepository customerRepositoryMock;
+
+	@InjectMocks
+	CustomerServiceImpl theService;
 
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
+	@Disabled
 	@Test
 	void testFindAll() {
 		fail("Not yet implemented");
 	}
 
+	@Disabled
 	@Test
 	void testFindById() {
 		fail("Not yet implemented");
 	}
 
+	@Disabled
 	@Test
 	void testSave() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	void testDeleteById() {
-		fail("Not yet implemented");
+		theService.deleteById(1);
+		verify(customerRepositoryMock).deleteById(1);
 	}
 
 }
