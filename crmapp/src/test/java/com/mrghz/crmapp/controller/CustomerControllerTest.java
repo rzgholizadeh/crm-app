b7c9e2d91a6642edaa5ce15c67b20f9f06f9c4a6
Reza <rzgholizadeh@gmail.com>
2020-02-27 21:23:20 -0700

    test: Refactor CustomerControllerTest
    Developed first version of test for save() and delete() methods.

--- crmapp/src/test/java/com/mrghz/crmapp/controller/CustomerControllerTest.java
+++ crmapp/src/test/java/com/mrghz/crmapp/controller/CustomerControllerTest.java
@@ -1,19 +1,28 @@
 package com.mrghz.crmapp.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
 import mrghz.crmapp.controller.CustomerController;
+import mrghz.crmapp.entity.Customer;
 import mrghz.crmapp.service.CustomerService;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("Customer Controller Test - ")
 class CustomerControllerTest {
+
+	private static final String REDIRECT_CUSTOMERS_LIST = "redirect:/customers/list";
 
 	@Mock
 	CustomerService customerServicelMock;
@@ -27,27 +36,30 @@ class CustomerControllerTest {
 
 	@Test
 	void testListCustomers() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	void testShowFormForAdd() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	void testShowFormForUpdate() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	void testDelete() {
-		fail("Not yet implemented");
+		// when
+		String viewName = theController.delete(1);
+		// then
+		assertThat(viewName).isEqualToIgnoringCase(REDIRECT_CUSTOMERS_LIST);
 	}
 
 	@Test
 	void testSaveCustomer() {
-		fail("Not yet implemented");
+		// when
+		String viewName = theController.saveCustomer(new Customer());
+		// then
+		assertThat(viewName).isEqualToIgnoringCase(REDIRECT_CUSTOMERS_LIST);
 	}
 
 }
